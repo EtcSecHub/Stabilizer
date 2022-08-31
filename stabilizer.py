48fc1d6326a21351f5b096086c7519e7ab99fa6c
EtcSecHub <106410560+EtcSecHub@users.noreply.github.com>
2022-08-31 07:26:14 -0400

    Update stabilizer.py
    Did some work on the findMethod() function; also, works on Kali now too.

--- stabilizer.py
+++ stabilizer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-# Shell Stabilizer v.20220823
+# Shell Stabilizer v.20220831
 # Requires nc and pexpect for proper function
 # You're welcome. -EtcSec
 
-# Finally got the stabilization to work "right" with support for ctrl-c and ctrl-z
-# Exiting is still ugly though; have to 'exit' twice, once through the -echo shell
-# A "Kill Switch" is set to the [ESC] key for a quick exit; sorry vim kids
-# Tested and working on Pop!_OS; was told that it does not work on Kali though.
-# Testing on Kali revealed that nc reports incoming connections differently
-# > Expecting 'received' on Kali does not work; needs to be 'Connection'
-# > Also, Kali does not have POSIX compliant symlinks to /bin/bash either
-# > This will have to be programaticaly accounted for later.
+# Works Pop!_OS and Kali now
+# Need to spend some time cleaning up and organizing
+# ZSH doesn't preserve the PS1 across the pty.spawn
+# > This causes some unwanted output on the screen (Line 70- expecting [token,'.'])
+# Need to add more shell and upgrade host lookup options
+# Can some kind of error detection be used to catch 'mishaps'?
 
 try: import pexpect
 except: print("\nStabilizer requires the pexpect module, please install it and try again.\n"); exit()
@@ -27,8 +25,14 @@ rTimeout = 20               # Response timeout value in seconds
 rSleep = 0                  # Sleep between commands, is this even needed?
 chkToken = ""               # Setup a "randomized" token
 for i in range(16): chkToken += choice(ascii_letters)
-# Grab terminal size
+
+# Grab some local syste enviroment things
 termSize = run(['stty','size'], capture_output=True).stdout.decode().strip().split()
+localBash = run(['which','bash'], capture_output=True).stdout.decode().strip()
+
+# Programs to look for on the remote system
+findShell = ['bash',]
+findUpgrade = ['python3',]
 
 # Command list, in order, to stabilize shell
 puntList = [
@@ -45,7 +49,7 @@ r'PS1="\n\u@\h:\w\n> "',
 ]
 
 def stabilize():  # Setup to receive connection via listener
-    rShell.expect('received')
+    rShell.expect(['received','connect'], timeout=10000)
     print("Future needs incoming IP:Port here...")
     print(f'Check Token is: {chkToken}')
     print("Attempting stabilization",end='',flush=True)
@@ -63,20 +67,49 @@ def stabilize():  # Setup to receive connection via listener
 def sendCmd(CMD): # Sends stabilizing commands & waits for "token" response
     try:
         rShell.sendline(CMD)
-        rShell.expect(chkToken, timeout=rTimeout)
+        rShell.expect([chkToken,'.'], timeout=rTimeout) # This makes a mess on the screen if ZSH
         sleep(rSleep)
     except:
         print("\nTimeout Reached; or somthing worse??\n")
         exit(1)
     print(".", end='', flush=True) # Visual indication of functionality
 
-def findMethod(): # Find a method for stabilization // INCOMPLETE; need to search system
+def findMethod(): # Find a method for stabilization // INCOMPLETE; needs more work
     # The "&& exit" at the end just tears down the call back shell on the way out
-    stabMethod = """python3 -c 'import pty;pty.spawn("/bin/bash")' && exit"""
+    shellPath = ""
+    upgradePath = ""
+
+    # Find Shell/Path on remote system, only checks /bin and /usr/bin for now
+    for shell in findShell:
+        rShell.sendline(f'which {shell}')
+        response = rShell.expect([f'/bin/{shell}',f'/usr/bin/{shell}']) 
+        if response == 0:
+            shellPath = f'/bin/{shell}'
+        elif response == 1:
+            shellPath = f'/usr/bin/{shell}'
+        else:
+            continue
+
+    # Find Upgrade/Path on remote system, only checks /bin and /usr/bin for now
+    for upgrade in findUpgrade:
+        rShell.sendline(f'which {upgrade}')
+        response = rShell.expect([f'/bin/{upgrade}',f'/usr/bin/{upgrade}']) 
+        if response == 0:
+            upgradePath = f'/bin/{upgrade}'
+        elif response == 1:
+            upgradePath = f'/usr/bin/{upgrade}'
+        else:
+            continue
+
+    # This needs to be an if-tree, in order of preference
+    if "python3" in upgradePath:
+        stabMethod = f"""{upgradePath} -c 'import pty;pty.spawn("{shellPath}")' && exit"""
+    
     return stabMethod
 
 def killEcho(): # Kill the echo
     # This block is messy, but works ;)
+    # Can this be cleaned up by using pexpect.sendcontrol()?
     getPidCommand = "ps a -o pid,command"
     OUTPUT = run(getPidCommand.split(' '), capture_output=True)
     output = OUTPUT.stdout.decode().split('\n')
@@ -101,14 +134,14 @@ if __name__ == "__main__":
         print("\nInvalid port number\n"); exit(1)
     
     # Spawn a new shell, start the listener, and announce listening state
-    rShell = pexpect.spawn(f'/bin/bash')
-    rShell.sendline(r'PS1="\n<No-Echo PseudoShell>\nPlease type exit press return to quit.\n> "')
+    rShell = pexpect.spawn(localBash)
+    rShell.sendline(r'PS1="\n<No Echo Garbage Shell>\nPlease type exit press return to quit.\n> "')
     rShell.sendline(f'{listenerCmd} {listenerPort}')
     print("[-] Listening on port:",listenerPort)
 
     # Do the thing
     stabilize()
-    rShell.interact(escape_character='\x1b') # [ESC] key will kill all and drop you out
-    
+    rShell.interact(escape_character='\x1b') # [ESC] key will kill it and drop you out
+
     # Close the shell that was created and reset the mess that was made when we're done
     system('reset')
